53d7e94b06e6fad27512b7c2d56654761d5d51db
Tanya Yeh <a0989160607@gmail.com>
2020-08-19 01:55:44 +0800

    first commit test web-project

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+document.querySelector('html').onclick = function(){
+    alert('mewo~~')
+}
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/kitten.jpg') {
+        myImage.setAttribute ('src','images/ragdoll.jpg');
+    }else {
+        myImage.setAttribute ('src','images/kitten.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName || myName === null) {
+        setUserName();
+    } else {
+      localStorage.setItem('name',myName);
+      myHeading.innerHTML = 'We love cats, '+ myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();    
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'We love cats, '+ storedName;
+}
+myButton.onclick = function () {
+    setUserName();
+}
